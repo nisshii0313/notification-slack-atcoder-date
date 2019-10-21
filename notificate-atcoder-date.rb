c34422c9e61560a11f42fee05117bace9e0e648f
EisKern <EisKern@users.noreply.github.com>
2019-10-21 13:55:46 +0900

    Manage hours instead of seconds

--- notificate-atcoder-date.rb
+++ notificate-atcoder-date.rb
@@ -17,14 +17,14 @@ contest_header = contests.shift
 
 # notification contests will be held today or tomorrow to slack
 now_time = Time.now
-now_time = Time.local(now_time.year, now_time.month, now_time.day, now_time.hour)
-notification_seconds_ago = [1 * 60 * 60, 24 * 60 * 60]
+now_time = Time.local(now_time.year, now_time.month, now_time.day, now_time.hour) # truncate minutes and seconds
+notification_hours_ago = [1, 24]
 contests.each do |contest|
     contest[0] = Time.parse(contest[0])
     held_time = contest[0]
-    notification_seconds_ago.each do |seconds_ago|
-        if now_time + seconds_ago == held_time
-            body = create_message_body(contest_header, contest, seconds_ago / 3600)
+    notification_hours_ago.each do |hours_ago|
+        if now_time + (hours_ago * 60 * 60) == held_time
+            body = create_message_body(contest_header, contest, hours_ago)
             response = https_request(ENV['SLACK_URL'], body)
         end
     end
